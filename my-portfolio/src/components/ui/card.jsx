0d77220107ec484d3309b58803dd799cac0ace1e
chandrakant teotia <chandrakantteotia@gmail.com>
2025-05-29 17:42:09 +0530

    Build production files and update Netlify config

--- my-portfolio/src/components/ui/card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Card = ({ children, className = "" }) => (
-  <div className={`bg-white rounded-xl shadow p-4 ${className}`}>{children}</div>
-);
-
-export const CardContent = ({ children, className = "" }) => (
-  <div className={`p-2 ${className}`}>{children}</div>
-);
--- my-portfolio/src/utils/dashboardService.js
+++
@@ -1,7 +0,0 @@
-// src/utils/dashboardService.js
-import axios from "./axiosInstance";
-
-export const getDashboardStats = async () => {
-  const response = await axios.get("/dashboard/stats");
-  return response.data;
-};
